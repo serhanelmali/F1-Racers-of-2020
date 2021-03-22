9d7d21d483cca06f924743d32d3c16229f4bbab1
serhan <serhanelmali@gmail.com>
2021-03-22 15:31:07 +0300

    pop up structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
+import { useEffect, useState } from "react";
+import { AllRacers, RaceDetails } from "./component/AllRacers";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const [races, setRaces] = useState([]);
+  const [winner, setWinner] = useState("");
+  const [choosenRace, setChoosenRace] = useState([]);
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    fetch(`https://ergast.com/api/f1/2020/results/1.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        setRaces(data.MRData.RaceTable.Races);
+      })
+      .catch((error) => console.log(error));
+
+    fetch(`https://ergast.com/api/f1/2020/driverStandings.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        const raceWinnerFirstName =
+          data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver
+            .givenName;
+        const raceWinnerLastName =
+          data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver
+            .familyName;
+        setWinner(`${raceWinnerFirstName} ${raceWinnerLastName}`);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>F1 Racers of 2020</h1>
+      {!clicked ? (
+        <>
+          <AllRacers
+            races={races}
+            winner={winner}
+            clicked={clicked}
+            onClickHandler={() => setClicked(true)}
+          />{" "}
+          <RaceDetails />
+        </>
+      ) : (
+        <RaceDetails
+          clicked={clicked}
+          onClickHandler={() => setClicked(false)}
+        />
+      )}
+    </div>
+  );
+};
 
 export default App;
